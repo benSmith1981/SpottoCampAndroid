47a50d3d012edd124c7d1850fb7d2ecd6c6d905c
Ben Smith <benjaminsmith1981@gmail.com>
2016-01-21 15:19:49 +0100

    threaded image loading with picasso

--- app/src/main/java/com/example/funkymonkey1981/spottocampandroid/SCListView.java
+++ app/src/main/java/com/example/funkymonkey1981/spottocampandroid/SCListView.java
@@ -6,9 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -20,12 +22,17 @@ public class SCListView extends ArrayAdapter<SCJSON.Spots.Data> {
 
     private LayoutInflater inflater;
     private ImageLoader mImageLoader;
+    Context context;
 
     public SCListView(Context context, @LayoutRes int layoutId, ArrayList<SCJSON.Spots.Data> campsites) {
         super(context, layoutId, campsites);
         inflater = LayoutInflater.from(context);
         mImageLoader = com.example.funkymonkey1981.spottocampandroid.SCImageLoader.getInstance().getImageLoader();
+        this.context = context;
 
+    }
+    public ImageLoader getmImageLoader() {
+        return mImageLoader;
     }
 
     @Override
@@ -49,8 +56,8 @@ public class SCListView extends ArrayAdapter<SCJSON.Spots.Data> {
 //        nv.setImageUrl(data.getThumbnail(), mImageLoader);
 
 
-//        ImageView imageView = (ImageView)  rowView.findViewById(R.id.img);
-//        Picasso.with(view.getContext()).load(data.getThumbnail()).into(imageView);
+        ImageView imageView = (ImageView)rowView.findViewById(R.id.img);
+        Picasso.with(rowView.getContext()).load(data.getThumbnail()).into(imageView);
 
         return rowView;
     }
